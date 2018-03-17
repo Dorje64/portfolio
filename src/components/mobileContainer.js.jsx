cc7094695ee62aa1e0d929c2162ea3bdd2194b9d
Dorje <ku.dorjee@gmail.com>
2018-03-17 21:34:15 +0545

    add icon to menu sidebar

--- src/components/mobileContainer.js.jsx
+++ src/components/mobileContainer.js.jsx
@@ -6,7 +6,10 @@ import {
   Menu,
   Responsive,
   Segment,
-  Sidebar
+  Sidebar,
+  Icon,
+  Container,
+  Button
 } from 'semantic-ui-react';
 
 class MobileContainer extends Component {
@@ -24,6 +27,7 @@ class MobileContainer extends Component {
     const { children } = this.props
     const { sidebarOpened } = this.state
 
+
     return (
       <Responsive {...Responsive.onlyMobile}>
         <Sidebar.Pushable>
@@ -38,6 +42,17 @@ class MobileContainer extends Component {
 
           <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handlePusherClick} style={{ minHeight: '100vh' }}>
             <Segment inverted textAlign='center' style={{ minHeight: 350, padding: '1em 0em' }} vertical>
+              <Container>
+                <Menu inverted pointing secondary size='large'>
+                  <Menu.Item onClick={this.handleToggle}>
+                    <Icon name='sidebar' />
+                  </Menu.Item>
+                  <Menu.Item position='right'>
+                    <Button as='a' inverted>Log in</Button>
+                    <Button as='a' inverted style={{ marginLeft: '0.5em' }}>Sign Up</Button>
+                  </Menu.Item>
+                </Menu>
+              </Container>
               <HomepageHeading mobile />
             </Segment>
 
